d4a9c32ab9ec591ec2ab761cf592d0412b46160c
hutchgrant <h.g.utchinson@gmail.com>
2015-03-25 05:05:05 -0400

    updating theme, forcing inverse nav, fixing theme, fixing coin image path

--- themes/CheckoutCrypto/template.php
+++ themes/CheckoutCrypto/template.php
@@ -3,3 +3,16 @@
  * @file
  * template.php
  */
+
+/**
+* Implements template_preprocess_page().
+*/
+function CheckoutCrypto_preprocess_page(&$vars) {
+  // Remove the "No front page content has been created yet.".
+  if (isset($vars['page']['content']['system_main']['default_message'])) {
+    unset($vars['page']['content']['system_main']['default_message']);
+  }
+ ///  Remove "Welcome to Drupal Message"
+ if (drupal_is_front_page()) {  drupal_set_title('');} 
+
+}
